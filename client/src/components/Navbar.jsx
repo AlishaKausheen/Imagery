bb9de91343d4122b6a6874439257b16e45fd02e7
AlishaKausheen <alishakausheen@gmail.com>
2024-04-19 07:54:50 -0700

    button change

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,15 +1,45 @@
 import Button from './button';
 import React from 'react'
 import styled from 'styled-components';
+import { AddRounded, ExploreRounded } from '@mui/icons-material';
+import { useNavigate, useLocation } from 'react-router-dom';
 
-
-const Container = styled.div``;
+const Container = styled.div`
+flex: 1;
+background: ${({ theme }) => theme.navbar};
+color: ${({ theme }) => theme.text_primary};
+font-weight: bold;
+font-size: 22px;
+padding: 14px 50px;
+display: flex;
+justify-content: space-between;
+align-items: center;
+box-shadow: 0 0 10px rgba(0,0,0,0.15);
+@media only screen and (max-width: 600px){
+  padding: 10px 12px;
+}
+`;
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const path = location.pathname.split("/");
   return (
       <Container>
-          Imagery
-          <Button text="Create new post"/>
+      Imagery
+      {path[1] === 'post' ? (
+      <Button
+          onClick={() => navigate('/')}
+          text="Explore post" leftIcon={<ExploreRounded style={{
+            fontSize: '18px',
+          }} />}
+        type='secondary'/>) :
+        (<Button
+          onClick={() => navigate('/post')}
+          text="Create new post" leftIcon={<AddRounded style={{
+            fontSize: '18px',
+          }} />} />
+        )}
     </Container>
   )
 }
