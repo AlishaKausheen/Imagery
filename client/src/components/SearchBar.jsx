caf13ca9dcd6335c34c9f79ef07135139839f90a
AlishaKausheen <alishakausheen@gmail.com>
2024-04-19 08:37:35 -0700

    home.jsx styling

---
+++ client/src/components/SearchBar.jsx
@@ -0,0 +1,32 @@
+import { SearchOutlined } from '@mui/icons-material';
+import React from 'react';
+import styled from 'styled-components';
+
+const SearchBarContainer = styled.div`
+max-width: 550px;
+display: flex;
+width: 90%;
+border: 1px solid ${({ theme }) => theme.text_secondary + 90};
+color: ${({theme})=> theme.text_primary};
+border-radius: 8px;
+padding: 12px 16px;
+cursor: pointer;
+gap: 6px;
+align-items: center;
+`;
+
+const SearchBar = () => {
+  return (
+      <SearchBarContainer>
+          <SearchOutlined/>
+          <input style={{
+              border: 'none',
+              outline: 'none',
+              width: '100%',
+              color: 'inherit'
+      }}/>
+    </SearchBarContainer>
+  )
+}
+
+export default SearchBar
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,16 +1,50 @@
 import React from 'react';
 import styled from 'styled-components';
+import SearchBar from '../components/SearchBar';
 
 
 const Container = styled.div`
 height: 100%;
 overflow-y:scroll;
+background: ${({theme})=> theme.bg};
+padding: 30px 30px;
+padding-bottom: 50px;
+display: flex;
+flex-direction: column;
+align-items: center;
+gap: 20px;
+@media (max-width: 768){
+  padding: 6px 10px;
+}
+`;
+
+const Headline = styled.div`
+font-size: 34px;
+font-weight: 500;
+color: ${({ theme }) => theme.text_primary};
+display: flex;
+align-items: center;
+flex-direction: column;
+@media (max-width: 600){
+  font-size: 24px;
+
+}
+`;
+
+const Span = styled.div`
+font-size: 34px;
+font-weight: 500;
+color: ${({ theme }) => theme.secondary};
+
 `;
 
 const Home = () => {
   return (
     <Container>
-      Home
+      <Headline>Explore popular posts in the Community!
+        <Span>◦Generate with AI◦</Span>
+      </Headline>
+      <SearchBar/>
     </Container>
   )
 }
