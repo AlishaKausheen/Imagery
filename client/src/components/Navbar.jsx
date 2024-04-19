2df87e7243cbdaedaac4c02bf29972d1e0042650
AlishaKausheen <alishakausheen@gmail.com>
2024-04-18 23:42:36 -0700

    router

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components';
+
+
+const Container = styled.div``;
+
+const Navbar = () => {
+  return (
+      <Container>
+          Navbar
+    </Container>
+  )
+}
+
+export default Navbar
