ed5f4930e1a76911a306c126df7c310882405c4a
AlishaKausheen <alishakausheen@gmail.com>
2024-04-20 00:02:37 -0700

    image card optimize 2

--- client/src/components/ImageCard.jsx
+++ client/src/components/ImageCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
+import { Avatar } from '@mui/material';
 
 const Card = styled.div`
 position: relative;
@@ -21,15 +22,42 @@ trasition: all 0.3s ease;
 `;
 
 const HoverOverlay = styled.div`
+opacity: 0;
+position: absolute;
+top: 0;
+left: 0;
+right: 0;
+bottom: 0;
+display: flex;
+flex-direction: column;
+align-items: start;
+gap: 2px;
+backdrop-filter: blur(2px);
+background: rgba(0,0,0, 0.5);
+color: ${({ theme }) => theme.white};
+transition: opacity 0.3s ease;
+border-radius: 6px;
+justify-content: end;
+padding: 12px;
 
+${Card}:hover & {
+    opacity: 1;
+}
 `;
 
 const Prompt = styled.div`
-
+font-weight: 400px;
+color: ${({ theme }) => theme.white};
+font-size: 15px;
 `;
 
 const Author = styled.div`
-
+font-weight: 600px;
+color: ${({ theme }) => theme.white};
+font-size: 14px;
+gap: 8px;
+display: flex;
+align-items: center;
 `;
 
 
@@ -39,7 +67,9 @@ const ImageCard = () => {
      <LazyLoadImage width="100%" src='https://th.bing.com/th/id/OIP.wzhvW5NxoZ24A4WZfvM3bQHaKb?rs=1&pid=ImgDetMain'/> 
           <HoverOverlay>
               <Prompt>Prompt</Prompt>
-              <Author>Alisha</Author>
+              <Author>
+                  <Avatar style={{width: '32px', height: '32px'}}>A</Avatar>
+                  Alisha</Author>
     </HoverOverlay>
       </Card>
   )
