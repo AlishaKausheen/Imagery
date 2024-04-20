e23c029dcbdb9884798ed523338816d380494d37
AlishaKausheen <alishakausheen@gmail.com>
2024-04-20 01:30:10 -0700

    generate image form structure

--- client/src/components/GenerateImageForm.jsx
+++ client/src/components/GenerateImageForm.jsx
@@ -1,17 +1,50 @@
 import React from 'react';
 import styled from 'styled-components';
+import Button from './button';
+import TextInput from './TextInput';
+import { AutoAwesome, CreateRounded } from '@mui/icons-material';
 
-const Form = styled.div``;
+const Form = styled.div`
+flex: 1;
+padding: 16px 20px;
+display: flex;
+flex-direction: column;
+gap: 9%;
+justify-content: center;
+`;
 
-const Top = styled.div` `;
+const Top = styled.div`
+display: flex;
+flex-direction: column;
+gap: 6px;
+`;
 
-const Title = styled.div``;
+const Title = styled.div`
+font-size: 28px;
+font-weight: 500;
+color: ${({ theme }) => theme.text_primary};
+`;
 
-const Desc = styled.div``;
+const Desc = styled.div`
+font-size: 17px;
+font-weight: 400;
+color: ${({ theme }) => theme.text_secondary};
+`;
 
-const Body = styled.div``;
+const Body = styled.div`
+display: flex;
+flex-direction: column;
+gap: 18px;
+font-size: 12px;
+font-weight: 400;
+color: ${({ theme }) => theme.text_secondary};
+`;
 
-const Actions = styled.div``;
+const Actions = styled.div`
+flex: 1;
+display: flex;
+gap: 8px;
+`;
 
 
 const GenerateImageForm = () => {
@@ -24,10 +57,23 @@ const GenerateImageForm = () => {
               <Desc>Write your prompt according to the image you want to generate</Desc>
           </Top>
           <Body>
-              Body
+              <TextInput
+                  label='Author'
+                  placeholder='Enter your name...'
+                  name='name'
+              />
+              <TextInput
+                  label='Prompt'
+                  placeholder='Write a detailed prompt about the image...'
+                  name='Prompt'
+                  rows='8'
+                  textArea
+              />
+              **You can post the AI generated image to the Community!!
           </Body>
           <Actions>
-              Actions
+              <Button text='Generate Image' flex leftIcon={<AutoAwesome />} />
+              <Button text='Post Image' type='secondary' flex leftIcon={<CreateRounded/>}/>
           </Actions>
     </Form>
   )
