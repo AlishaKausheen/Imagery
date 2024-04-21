b0e6d4610b0c16305fa076d9d72dfc1bbdf8dbb0
AlishaKausheen <alishakausheen@gmail.com>
2024-04-21 04:50:03 -0700

    secure

--- server/controllers/Posts.js
+++ server/controllers/Posts.js
@@ -7,11 +7,7 @@ import { v2 as cloudinary } from "cloudinary";
 
 dotenv.config();
           
-//cloudinary.config({ 
-  //cloud_name: 'dftwu3ocy', 
-  //api_key: '551575543784676', 
-  //api_secret: 'phjLlVyu6Uwjmu5a0YLCbYquRyg' 
-//});
+
 
 //get all posts
 export const getAllPosts = async (req, res, next) => {
@@ -19,6 +15,6 @@ export const getAllPosts = async (req, res, next) => {
         const posts = await Post.find({});
         return res.status(200).json({ success: true, data: posts });
     } catch (error){
-        next(createError(error.status, error?.response.data?.error?.message || error?.message))
+       // next(createError(error.status, error?.response.data?.error?.message || error?.message))
     }
 }
