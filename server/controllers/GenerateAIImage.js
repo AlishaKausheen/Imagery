4ab6198961261dec49dc27a5918440ed898cc76e
AlishaKausheen <alishakausheen@gmail.com>
2024-04-25 22:14:07 -0700

    open ai config

--- server/controllers/GenerateAIImage.js
+++ server/controllers/GenerateAIImage.js
@@ -1,6 +1,11 @@
 import * as dotenv from "dotenv";
 import { createError } from "../error.js";
+import { Configuration } from "openai";
 
 dotenv.config();
 
 //Set up Open AI API Key
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+})
+
