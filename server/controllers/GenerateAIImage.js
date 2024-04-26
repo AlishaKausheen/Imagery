236617bd4e80edb817b82ae25ea190e79c1b336e
AlishaKausheen <alishakausheen@gmail.com>
2024-04-25 23:01:59 -0700

    image json

--- server/controllers/GenerateAIImage.js
+++ server/controllers/GenerateAIImage.js
@@ -2,6 +2,7 @@ import * as dotenv from "dotenv";
 import { createError } from "../error.js";
 import { Configuration, OpenAIApi } from "openai";
 
+
 dotenv.config();
 
 //Set up Open AI API Key
@@ -16,7 +17,14 @@ const openai = new OpenAIApi(configuration);
 export const generateImage = async (req, res, next) => {
     try {
         const { prompt } = req.body;
-
+        const response = await openai.createImage({
+            prompt,
+            n: 1,
+            size: "1024x1024",
+            response_format: "b64json",
+        });
+        const generateImage = response.data.data[0].b64_json;
+        return res.status(200).json({ photo: generateImage });
     } catch (error) {
        next(
             createError(
